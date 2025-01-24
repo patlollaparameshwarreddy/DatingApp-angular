1143e12b873c89a389373c35f7812960d709b770
patlollaparameshwarreddy <patlollaparameshwarreddy7@gmail.com>
2025-01-24 22:19:21 +0530

    angular project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  Users:any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next : response => this.Users = response,
+      error : error => console.error(error),
+      complete : () => console.log('Request has completed')
+      
+    })
+  }
 }
