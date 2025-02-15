ea1c037f8cca8a0f4be88de6a440b2f2bf539f22
patlollaparameshwarreddy <patlollaparameshwarreddy7@gmail.com>
2025-02-15 23:05:46 +0530

    authguard, bootswatch and toaster implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MembersListComponent } from './members/members-list/members-list.component';
+import { MembersDetailComponent } from './members/members-detail/members-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {
+        path:'',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children:[
+            {path:'members', component:MembersListComponent, canActivate:[authGuard]},
+            {path:'members/:id', component:MembersDetailComponent},
+            {path:'lists', component:ListsComponent},
+            {path:'messages', component:MessagesComponent}
+        ]
+    },
+    {path:'**', component:HomeComponent, pathMatch:'full'}
+];
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, output} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,6 +13,7 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent {
 cancelRegister = output<boolean>();
 private accountService = inject(AccountService);
+private toaster = inject(ToastrService);
 model: any = {};
 
 register(){
@@ -20,7 +22,7 @@ register(){
       console.log(response);
       this.cancel();
     },
-    error: error => console.log(error)
+    error: error => this.toaster.error(error.error)
   })
 }
 
